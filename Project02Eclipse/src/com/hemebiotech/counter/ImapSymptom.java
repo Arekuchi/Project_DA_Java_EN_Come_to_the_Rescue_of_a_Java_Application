da649c640902b5322bb40f9c174a440298216b7e
Alexis Berthelot <berthelota2@gmail.com>
2020-07-10 16:57:02 +0200

    package class interface counter

---
+++ Project02Eclipse/src/com/hemebiotech/counter/ImapSymptom.java
@@ -0,0 +1,9 @@
+package com.hemebiotech.counter;
+
+import java.util.List;
+import java.util.Map;
+
+public interface ImapSymptom {
+	Map<String, Integer> count(List<String> symptoms);
+
+}
---
+++ Project02Eclipse/src/com/hemebiotech/counter/mapSymptom.java
@@ -0,0 +1,23 @@
+package com.hemebiotech.counter;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class mapSymptom implements ImapSymptom {
+
+	@Override
+	public Map<String, Integer> count(List<String> symptoms) {
+		Map<String, Integer> myMap = new HashMap<>();
+		for (String symptom : symptoms) {
+			if (myMap.containsKey(symptom)) {
+				myMap.put(symptom, myMap.get(symptom) + 1);
+			} else {
+				myMap.put(symptom, 1);
+			}
+
+		}
+		return myMap;
+	}
+
+}
